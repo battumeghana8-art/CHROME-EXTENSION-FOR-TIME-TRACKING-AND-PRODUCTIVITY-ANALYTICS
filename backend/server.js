ad99847583193e50e774f0bd8ef12af79e5b771c
battumeghana8-art <battumeghana8@gmail.com>
2026-06-23 22:54:14 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  "/api/usage",
+  require("./routes/usageRoutes")
+);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
---
+++ backend/usage.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const usageSchema = new mongoose.Schema({
+  website: String,
+  duration: Number,
+  category: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model(
+  "Usage",
+  usageSchema
+);
