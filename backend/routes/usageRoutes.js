b2c599712517e366cfb8f4b9d1539953a79e38ce
battumeghana8-art <battumeghana8@gmail.com>
2026-06-23 22:55:53 +0530

    Add files via upload

--- backend/routes/usageRoutes.js
+++ backend/routes/usageRoutes.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
 
+router.post("/", (req, res) => {
+
+  const data =
+    JSON.parse(
+      fs.readFileSync("./data.json")
+    );
+
+  data.push(req.body);
+
+  fs.writeFileSync(
+    "./data.json",
+    JSON.stringify(data, null, 2)
+  );
+
+  res.json({
+    success: true
+  });
+});
+
+router.get("/", (req, res) => {
+
+  const data =
+    JSON.parse(
+      fs.readFileSync("./data.json")
+    );
+
+  res.json(data);
+});
+
+module.exports = router;
