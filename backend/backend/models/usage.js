1b0e1cb901a6f1d09b915955dd645693af0f84ac
battumeghana8-art <battumeghana8@gmail.com>
2026-06-23 22:56:59 +0530

    Add files via upload

--- backend/backend/models/usage.js
+++ backend/backend/models/usage.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+const usageSchema =
+new mongoose.Schema({
+  website:String,
+  duration:Number,
+  category:String,
+  date:{
+      type:Date,
+      default:Date.now
+  }
+});
+
+module.exports =
+mongoose.model(
+"Usage",
+usageSchema
+);
